cd80aa1e4641a9bea4be684fac7d2c152b4f7b6d
Byshov <byshov1998@gmail.com>
2026-02-15 18:47:51 +0100

    done task 11-th of JS

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,17 +1,72 @@
-export function createGallery(images){
-  return `
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-  `
+// import '../css/styles.css'
+
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+let refreshGallery = null;
+
+export function createGallery(images) {
+  let markup = images
+      .map(
+        ({
+           webformatURL,
+           largeImageURL,
+           tags,
+           likes,
+           views,
+           comments,
+           downloads,
+         }) => {
+          return `
+              <li class="list-item">
+                <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" /></a>
+                <div class="list-content">
+                  <div class="list-content-item">
+                    <h2 class="name">Likes</h2>
+                    <p class="value">${likes}</p>
+                  </div>
+                  <div class="list-content-item">
+                    <h2 class="name">Views</h2>
+                    <p class="value">${views}</p>
+                  </div>
+                  <div class="list-content-item">
+                    <h2 class="name">Comments</h2>
+                    <p class="value">${comments}</p>
+                  </div>
+                  <div class="list-content-item">
+                    <h2 class="name">Downloads</h2>
+                    <p class="value">${downloads}</p>
+                  </div>
+                </div>
+              </li>
+            `;
+        },
+      )
+      .join('');
+
+  gallery.innerHTML = markup;
+  if (refreshGallery) {
+    refreshGallery.refresh();
+  } else {
+    refreshGallery = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
 }
 
-export function clearGallery(){
-
+export function clearGallery() {
+  gallery.innerHTML = '';
 }
 
-export function showLoader(){
-
+export function showLoader() {
+  loader.style.display = 'inline-block';
 }
 
-export function hideLoader(){
-
+export function hideLoader() {
+  loader.style.display = 'none';
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,48 @@
-import "./js/pixabay-api"
+import { getImageByQuery } from './js/pixabay-api';
+import { createGallery } from './js/render-functions';
+import { clearGallery } from './js/render-functions';
+import { showLoader } from './js/render-functions';
+import { hideLoader } from './js/render-functions';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
+const form = document.querySelector('.form');
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchInput = form.elements['search-text'].value.trim();
+
+  if(searchInput === '') {
+    iziToast.show({
+      message: 'Please enter a search query!',
+      backgroundColor: `#EF4040`,
+      messageColor: `#ffffff`,
+      position: `topRight`,
+      maxWidth: `432px`,
+    });
+    return;
+  }
+  clearGallery();
+  showLoader();
+
+  getImageByQuery(searchInput).then(data =>{
+    console.log(data.hits);
+    if(!data.hits || data.hits.length === 0) {
+      iziToast.show({
+        message:
+          ' Sorry, there are no images matching your search query. Please try again!',
+        backgroundColor: `#EF4040`,
+        messageColor: `#ffffff`,
+        position: `topRight`,
+        maxWidth: `432px`,
+      })
+      hideLoader();
+      return;
+    }
+    createGallery(data.hits);
+    hideLoader();
+  }).catch(e => {
+    hideLoader();
+    console.log(e);
+  });
+});
